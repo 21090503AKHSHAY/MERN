63a55be572a0710d4787e92640cc9af196d7122f
21090503AKHSHAY <akhshaytp21@gmail.com>
2025-01-19 20:52:33 +0530

    Day6_Task

---
+++ Day6_Task/contact.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+const Contact = () => {
+  return (
+    <div>
+      <h2>Contact Information</h2>
+      <p>Email: contact@example.com</p>
+    </div>
+  );
+};
+export default Contact;
