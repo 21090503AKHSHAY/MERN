a49b8f2dfe720102eb2e7d7ddd95e009244c8073
21090503AKHSHAY <akhshaytp21@gmail.com>
2025-01-23 20:12:53 +0530

    Day7_AN

---
+++ Day7_AN/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import ContactState from './components/ContactState';
+import Gallery from './components/Gallery';
+
+function App() {
+
+  return (
+    <>
+      <Gallery/>
+      <ContactState/>
+    </>
+  )
+}
+
+export default App
---
+++ Day7_AN/src/components/Gallery.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Benny from '../assets/monkey.jpg'
+{/*we can use different names for props*/}
+const Gallery = (props) => {
+  return (
+    <div>
+        {/*if there is no value it will not show any error */}
+      <h1>This is a {props.page} page</h1>
+      <img src={Benny} alt = ""  />
+      <p>Image is {props.image}</p>
+    </div>
+  )
+}
+
+export default Gallery
